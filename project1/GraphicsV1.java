2b9f831a23eedd19f09f084dfdb271de12edc627
Yeshi <yeshic9@gmail.com>
2015-05-12 10:07:57 -0500

    Added classes

---
+++ project1/GraphicsV1.java
@@ -0,0 +1,17 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Class GraphicsV1 - write a description of the class here
+ * 
+ * @author (your name) 
+ * @version (a version number)
+ */
+public class GraphicsV1 extends JApplet
+{
+    public void paint(Graphics f)
+    {
+        f.drawLine(0,300,500,300);
+    }   
+}
