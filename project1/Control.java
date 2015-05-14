45b6f782e9e89b3157af5b8f75385d969142af81
Danielle Fang <daniellefang1@gmail.com>
2015-05-14 09:36:27 -0500

    Changed control class

--- project1/Control.java
+++ project1/Control.java
@@ -1,33 +1,74 @@
 
 /**
  * Write a description of class Control here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The control class controls the movement of a sprite
+ * @author (Danielle) 
+ * @version (5/5/15)
  */
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 public class Control
 {
-    // instance variables - replace the example below with your own
+    private String sprite = "TempMario.png";
+    private int dx;
+    private int dy;
     private int x;
+    private int y;
+    private Image image;
 
-    /**
-     * Constructor for objects of class Control
-     */
     public Control()
     {
-        // initialise instance variables
-        x = 0;
+        ImageIcon icon = new ImageIcon(this.getClass().getResource(sprite));
+        image = icon.getImage();
+        x = 300;
+        y = 300;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    public void move()
     {
-        // put your code here
-        return x + y;
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() 
+    {
+        return x;
+    }
+
+    public int getY() 
+    {
+        return y;
+    }
+
+    public Image getImage() 
+    {
+        return image;
+    }
+
+    public void keyPressed(KeyEvent k) 
+    {
+        int key = k.getKeyCode();
+        if (key == KeyEvent.VK_LEFT)
+            dx = -1;
+        if (key == KeyEvent.VK_RIGHT)
+            dx = 1;
+        if (key == KeyEvent.VK_UP)
+            dy = -1;
+        if (key == KeyEvent.VK_DOWN)
+            dy = 1;
+    }
+
+    public void keyReleased(KeyEvent k) 
+    {
+        int key = k.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) 
+            dx = 0;
+        if (key == KeyEvent.VK_RIGHT) 
+            dx = 0;
+        if (key == KeyEvent.VK_UP)
+            dy = 0;
+        if (key == KeyEvent.VK_DOWN)
+            dy = 0;
     }
 }
